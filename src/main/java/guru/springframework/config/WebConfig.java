057876a6cd6770b3e8e1e230728ac4d2dc595cda
petrovale <leha.isakov@yandex.ru>
2019-07-06 17:25:14 +0300

    adding RouterFunction Example

---
+++ src/main/java/guru/springframework/config/WebConfig.java
@@ -0,0 +1,26 @@
+package guru.springframework.config;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  RouterFunction<?> routes(RecipeService recipeService){
+    return RouterFunctions.route(GET("/api/recipes"),
+        serverRequest -> ServerResponse
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(recipeService.getRecipes(), Recipe.class));
+
+  }
+}
