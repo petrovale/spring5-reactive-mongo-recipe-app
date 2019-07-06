826e2ea0a6cc0a528cf5f0f377404d00dd6aa6a0
petrovale <leha.isakov@yandex.ru>
2019-07-06 14:17:20 +0300

    adding validation for ingredients

--- src/main/java/guru/springframework/commands/IngredientCommand.java
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -1,5 +1,8 @@
 package guru.springframework.commands;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,7 +18,11 @@ import java.math.BigDecimal;
 public class IngredientCommand {
     private String id;
     private String recipeId;
+    @NotBlank
     private String description;
+    @NotNull
+    @Min(1)
     private BigDecimal amount;
+    @NotNull
     private UnitOfMeasureCommand uom;
 }
